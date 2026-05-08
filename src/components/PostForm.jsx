316cfc7347ed2e183b86923bc10720dd58f9f3b2
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-05-08 15:13:31 +0200

    ADD struttura html

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -11,10 +11,34 @@
 import styles from "./PostForm.module.css";
 
 function PostForm() {
-    return (
-        <div className="container vh-100 d-flex justify-content-center align-items-center">
+    return <>
+        <div className="container d-flex justify-content-center align-items-center">
             <h1 className={`${styles.title} text-primary`}>Post Form</h1>
         </div>
-    );
+        <div className="container d-flex justify-content-center align-items-center">
+            <form>
+                {/* Nome autore */}
+                <div>
+                    <label htmlFor="autore">Autore</label>
+                    <input type="text" placeholder="Inserire il nome dell'autore" />
+                </div>
+                {/* Titolo del post */}
+                <div>
+                    <label htmlFor="titolo">Titolo</label>
+                    <input type="text" placeholder="Inserire il titolo del post" />
+                </div>
+                {/* Corpo del testo */}
+                <div>
+                    <label htmlFor="corpo">Contenuto</label>
+                    <textarea type="text" rows="5" placeholder="Inserire il contenuto del post" />
+                </div>
+                {/* Pubblico o bozza */}
+                <div>
+                    <label htmlFor="pubblico">Pubblico</label>
+                    <input type="checkbox"/>
+                </div>
+            </form>
+        </div>
+    </>;
 }
 export default PostForm;
