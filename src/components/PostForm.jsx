c33b9f89ab306ebdbb2514005d6813b6d2e15cf5
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-05-08 15:17:50 +0200

    ADD const API

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,16 +1,12 @@
-// Esempio di utilizzo di un CSS Module.
-//
-// Importando il file ".module.css" come oggetto `styles`, possiamo
-// applicare le sue regole con la sintassi `className={styles.nomeClasse}`.
-// Le classi così applicate rimangono "scoped" al componente, evitando
-// collisioni con altri stili globali (per esempio quelli di Bootstrap).
-//
-// Per combinare una classe del modulo con altre classi (globali o di
-// Bootstrap) si usa un template literal, come nell'`<h1>` qui sotto.
-
 import styles from "./PostForm.module.css";
 
+const API_URL = 'https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts';
+
+
 function PostForm() {
+
+
+
     return <>
         <div className="container d-flex justify-content-center align-items-center">
             <h1 className={`${styles.title} text-primary`}>Post Form</h1>
