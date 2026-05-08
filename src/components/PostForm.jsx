94c1f65f7810a233c377be155966fc4240fda4aa
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-05-08 15:32:05 +0200

    SET initial data & formdata useState

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -2,9 +2,15 @@ import styles from "./PostForm.module.css";
 
 const API_URL = 'https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts';
 
+const formDataInitial = {
+    author:'',
+    title:'',
+    body:'',
+    public: false
+};
 
 function PostForm() {
-
+    const [formData, setFormData] = useState(formDataInitial);
 
 
     return <>
